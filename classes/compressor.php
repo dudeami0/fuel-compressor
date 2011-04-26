205dc3107c62678dbeab542618a80bc4ac2170b2
Kris Keyes <dudeami0@gmail.com>
2011-04-25 23:38:20 -0400

    Changed calls to \Fuel\Core to root namespace.

--- classes/compressor.php
+++ classes/compressor.php
@@ -34,8 +34,8 @@ class Compressor {
 	 */
 	function js($input, $filename = null, $force = false) {
 		// To make sure production runs well, return the asset tag.
-		if (\Fuel\Core\Config::get('environment') === \Fuel::PRODUCTION)
-			return \Fuel\Core\Asset::js($filename.'.js');
+		if (\Config::get('environment') === \Fuel::PRODUCTION)
+			return \Asset::js($filename.'.js');
 		// Now we continue like normal
 		$code = '';
 		$return = $path = null;
@@ -44,7 +44,7 @@ class Compressor {
 				// Inital loop
 				for ($i = 0; $i < count($input); $i++) {
 					// Get the location
-					$input[$i] = \Fuel\Core\Asset::find_file($input[$i], 'js');
+					$input[$i] = \Asset::find_file($input[$i], 'js');
 					// If we need to make the path,
 					if (is_null($path) && $input[$i] !== false) {
 						$pos = strrpos($input[0], '/') > strrpos($input[0], '\\') ? strrpos($input[0], '/') : strrpos($input[0], '\\');
@@ -72,9 +72,9 @@ class Compressor {
 		$code = $packer->pack();
 		if ($filename !== null && !empty($code)) {
 			$return = file_put_contents($path.$filename.'.js', $code);
-			$return = $return !== false ? \Fuel\Core\Asset::js($filename . '.js') : false;
+			$return = $return !== false ? \Asset::js($filename . '.js') : false;
 		} else if ($filename !== null && is_file($path.$filename.'.js')) {
-			$return = \Fuel\Core\Asset::js($filename . '.js');
+			$return = \Asset::js($filename . '.js');
 		}
 		return is_null($return) ? $code : $return;
 	}
@@ -92,8 +92,8 @@ class Compressor {
 	 */
 	function css($input, $filename = null, $force = false) {
 		// To make sure production runs well, return the asset tag.
-		if (\Fuel\Core\Config::get('environment') === \Fuel::PRODUCTION)
-			return \Fuel\Core\Asset::css($filename.'.css');
+		if (\Config::get('environment') === \Fuel::PRODUCTION)
+			return \Asset::css($filename.'.css');
 		// Now we continue like normal
 		$code = '';
 		$return = $path = null;
@@ -102,7 +102,7 @@ class Compressor {
 				// Inital loop
 				for ($i = 0; $i < count($input); $i++) {
 					// Get the location
-					$input[$i] = \Fuel\Core\Asset::find_file($input[$i], 'css');
+					$input[$i] = \Asset::find_file($input[$i], 'css');
 					// If we need to make the path,
 					if (is_null($path) && $input[$i] !== false) {
 						$pos = strrpos($input[0], '/') > strrpos($input[0], '\\') ? strrpos($input[0], '/') : strrpos($input[0], '\\');
@@ -129,9 +129,9 @@ class Compressor {
 		$code = CssMin::minify($code);
 		if ($filename !== null && !empty($code)) {
 			$return = file_put_contents($path.$filename.'.css', $code);
-			$return = $return !== false ? \Fuel\Core\Asset::css($filename . '.css') : false;
+			$return = $return !== false ? \Asset::css($filename . '.css') : false;
 		} else if ($filename !== null && is_file($path.$filename.'.css')) {
-			$return = \Fuel\Core\Asset::css($filename . '.css');
+			$return = \Asset::css($filename . '.css');
 		}
 		return is_null($return) ? $code : $return;
 	}
